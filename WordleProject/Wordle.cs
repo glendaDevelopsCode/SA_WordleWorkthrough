ae7cc53a8a172ca93ee7c600a7963b4ee1bc86f6
Nat (Glenda) <glenda.miyoshi@gmail.com>
2026-03-23 15:00:24 -0500

    holy moses, that is a lot of work with no commit. Let me  get this backed up.

--- WordleProject/Wordle.cs
+++ WordleProject/Wordle.cs
@@ -1,4 +1,23 @@
 public class Wordle
 {
-    
+    public string validateSecretWord(string SecretWordToTest)
+    {
+        if (SecretWordToTest == string.Empty) return "Invalid Secret Word: No Word Supplied";
+        if (SecretWordToTest.Length < 5) return "Invalid Secret Word: Word too Short";
+        if (SecretWordToTest.Length > 5) return "Invalid Secret Word: Word too long";
+        if (SecretWordToTest.Any(char.IsDigit)) return "Invalid Secret Word: Word should have only letters";
+
+        return "Let's Play!";
+    }
+
+    public string validateGuess (string guessedWord, string secretWordToTest)
+    {
+        if (guessedWord == string.Empty) return "Invalid Guess: No Word Supplied";
+        
+        //if (guessedWord == secretWordToTest) return "GGGGG";
+
+
+        return "-----";
+    }
+
 }
--- WordleProjectTest/WordleTests.cs
+++ WordleProjectTest/WordleTests.cs
@@ -2,5 +2,184 @@ namespace WordleProjectTest;
 
 public class WordleProjectTest
 {
+
+    #region "SecretWord Validation"
+     [Fact]
+    public void Should_ReturnInvalid_NoSecretWord()
+    {
+        //Arrange
+        var secretWordToTest = "";
+        Wordle wordle = new();
+
+        //Act
+        var result = wordle.validateSecretWord(secretWordToTest);
+
+        //Assert
+        Assert.Equal("Invalid Secret Word: No Word Supplied", result);
+    }
+
+    [Fact]
+    public void Should_ReturnInvalid_TooShortSecretWord()
+    {
+        //Arrange
+        var secretWordToTest = "no";
+        Wordle wordle = new();
+
+        //Act
+        var result = wordle.validateSecretWord(secretWordToTest);
+
+        //Assert
+        Assert.Contains("Invalid Secret Word: Word too Short", result);
+    }
+
+    [Fact]
+    public void Should_ReturnInvalid_TooLongSecretWord()
+    {
+        //Arrange
+        var secretWordToTest = "thisisnotrighteither";
+        Wordle wordle = new();
+
+        //Act
+        var result = wordle.validateSecretWord(secretWordToTest);
+
+        //Assert
+        Assert.Contains("Invalid Secret Word: Word too long", result);
+
+    }
+
+    [Fact]
+    public void Should_ReturnInvalid_NoDigitsInSecretWord()
+    {
+        //Arrange
+        var secretWordToTest = "stop1";
+        Wordle wordle = new();
+
+        //Act
+        var result = wordle.validateSecretWord(secretWordToTest);
+
+        //Assert
+        Assert.Contains("Invalid Secret Word: Word should have only letters", result);
+    }
+
+    [Fact]
+    public void Should_ReturnValid_SecretWord()
+    {
+        //Arrange
+        var secretWordToTest = "plane";
+        Wordle wordle = new();
+
+        //Act
+        var result = wordle.validateSecretWord(secretWordToTest);
+
+        //Assert
+        Assert.Contains("Let's Play!", result);
+    }
     
+    #endregion
+
+    #region "Game Logic"
+
+     [Fact]
+    public void Should_ReturnInvalid_NoGuess()
+    {
+        //Arrange
+        var secretWordToTest = "plane";
+        var guessedWord = "";
+        Wordle wordle = new();
+
+        //Act
+        wordle.validateSecretWord(secretWordToTest);
+        var result = wordle.validateGuess(guessedWord, secretWordToTest);
+
+        //Assert
+        Assert.Equal("Invalid Guess: No Word Supplied", result);
+     }
+
+    // public void Should_ReturnInvalid_ShortGuess()
+    // {
+    //     //Arrange
+    //     var secretWordToTest = "plane";
+    //     var guessedWord = "";
+    //     //Act
+    //     //Assert
+
+    //  }
+
+    // public void Should_ReturnInvalid_LongGuess()
+    // {
+    //     //Arrange
+    //     var secretWordToTest = "plane";
+    //     var guessedWord = "";
+
+    //     //Act
+    //     //Assert
+    //  }
+
+    // public void Should_ReturnInvalid_DigitGuess()
+    // {
+    //     //Arrange
+    //     var secretWordToTest = "plane";
+    //     var guessedWord = "";
+    //     //Act
+    //     //Assert
+
+    // }
+
+    // public void Should_ReturnInvalid_Guess()
+    // {
+    //    //Arrange
+    //     var secretWordToTest = "plane";
+    //     var guessedWord = "";
+
+    //     //Act
+
+    //     //Assert
+
+    // }
+
+    // public void Should_ReturnValid_CorrectGuess()
+    // {
+    //     //Arrange
+
+    //     //Act
+
+    //     //Assert       
+    // }
+
+    // public void Should_ReturnValid_IncorrectGuess()
+    // {
+    //     //Arrange
+
+    //     //Act
+
+    //     //Assert        
+    // }
+
+    // public void Should_ReturnValid_PartialCorrectGuess()
+    // {
+    //      //Arrange
+
+    //     //Act
+
+    //     //Assert       
+    // }
+
+    // public void Should_ReturnValid_PartialCorrectMultiLetterGuess()
+    // {
+    //     //Arrange
+
+    //     //Act
+
+    //     //Assert        
+    // }
+
+    // public void Should_ReturnValid_PartialCorrectLetterMutlipledGuess()
+    // {
+    //     //Arrange
+
+    //     //Act
+
+    //     //Assert        
+    // }
+    #endregion
 }
