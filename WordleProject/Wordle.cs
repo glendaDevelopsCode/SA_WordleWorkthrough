12f76f48e64e1764a0d060b911f30839c6dde5ba
Nat (Glenda) <glenda.miyoshi@gmail.com>
2026-03-23 22:05:35 -0500

    THIS WORKS! :D

--- WordleProject/Wordle.cs
+++ WordleProject/Wordle.cs
@@ -2,7 +2,7 @@ namespace WordleProject;
 
 public class Wordle
 {
-    public string validateSecretWord(string SecretWordToTest)
+    public string ValidateSecretWord(string SecretWordToTest)
     {
         if (SecretWordToTest == string.Empty) return "Invalid Secret Word: No Word Supplied";
         if (SecretWordToTest.Length < 5) return "Invalid Secret Word: Word too Short";
@@ -12,7 +12,7 @@ public class Wordle
         return "Let's Play!";
     }
 
-    public string validateGuess (string guessedWord, string secretWordToTest)
+    public string ValidateGuess (string guessedWord, string secretWordToTest)
     {
         if (guessedWord == string.Empty) return "Invalid Guess: No Word Supplied";
         if (guessedWord.Length < 5) return "Invalid Guess: guess too short";
@@ -22,19 +22,21 @@ public class Wordle
         if (guessedWord == secretWordToTest) return "GGGGG";
 
         string yourGuess = "";
+        List<char> goodGuesses = [];
 
-        for (var i = 0; i < guessedWord.Count(); i++)
+        for (var i = 0; i < guessedWord.Length; i++)
         {
 
             if (guessedWord[i] == secretWordToTest[i]) 
             {                
                 yourGuess += 'G';
+                goodGuesses.Add(guessedWord[i]);
                 continue;
             }
             yourGuess += '-'; 
         }
 
-        for (var i = 0; i < guessedWord.Count(); i++)
+        for (var i = 0; i < guessedWord.Length; i++)
         {
             if (yourGuess[i] == 'G') continue;
 
@@ -42,26 +44,17 @@ public class Wordle
             {
                 //This variable stores a partial word, it's everything that we have already tried.
                 var alreadyTested = guessedWord.Substring(0, i);
-                //This is the number of times the letter has already come up.
-                var numberInAlreadyTested = alreadyTested.Count(x => x == guessedWord[i]);
 
+                //This is the number of times the letter has already come up.                                
+                var numberInAlreadyTested = alreadyTested.Count(x => x == guessedWord[i]);
+                int numberOfClearedCorrectInstances = goodGuesses.Count(x=>x == guessedWord[i]); 
+                               
                 //This is the total number of times the guessed letter occurs in the word.
                 var numberTotalOccurrances = secretWordToTest.Count(x => x == guessedWord[i]);
-                
-                if ( numberTotalOccurrances <= numberInAlreadyTested )
-                {
-                    yourGuess = yourGuess.Remove(i, 1).Insert(i, "-") ;
-                }
-                else
-                {
-                    yourGuess = yourGuess.Remove(i, 1).Insert(i,"Y"); 
-                }
 
-                continue;
+                if (!( (numberTotalOccurrances - numberOfClearedCorrectInstances) <= numberInAlreadyTested )) yourGuess = yourGuess.Remove(i, 1).Insert(i,"Y"); 
             }
-            yourGuess = yourGuess.Remove(i,1).Insert(i, "-"); 
         }
-
         return yourGuess;
     }
 
--- WordleProjectTest/WordleTests.cs
+++ WordleProjectTest/WordleTests.cs
@@ -8,42 +8,42 @@ public class WordleProjectTest
      [Fact]
     public void Should_ReturnInvalid_NoSecretWord()
     {
-        //Arrange
+        
         var secretWordToTest = "";
         Wordle wordle = new();
 
-        //Act
-        var result = wordle.validateSecretWord(secretWordToTest);
+        
+        var result = wordle.ValidateSecretWord(secretWordToTest);
 
-        //Assert
+        
         Assert.Equal("Invalid Secret Word: No Word Supplied", result);
     }
 
     [Fact]
     public void Should_ReturnInvalid_TooShortSecretWord()
     {
-        //Arrange
+        
         var secretWordToTest = "no";
         Wordle wordle = new();
 
-        //Act
-        var result = wordle.validateSecretWord(secretWordToTest);
+        
+        var result = wordle.ValidateSecretWord(secretWordToTest);
 
-        //Assert
+        
         Assert.Contains("Invalid Secret Word: Word too Short", result);
     }
 
     [Fact]
     public void Should_ReturnInvalid_TooLongSecretWord()
     {
-        //Arrange
+        
         var secretWordToTest = "thisisnotrighteither";
         Wordle wordle = new();
 
-        //Act
-        var result = wordle.validateSecretWord(secretWordToTest);
+        
+        var result = wordle.ValidateSecretWord(secretWordToTest);
 
-        //Assert
+        
         Assert.Contains("Invalid Secret Word: Word too long", result);
 
     }
@@ -51,28 +51,28 @@ public class WordleProjectTest
     [Fact]
     public void Should_ReturnInvalid_NoDigitsInSecretWord()
     {
-        //Arrange
+        
         var secretWordToTest = "stop1";
         Wordle wordle = new();
 
-        //Act
-        var result = wordle.validateSecretWord(secretWordToTest);
+        
+        var result = wordle.ValidateSecretWord(secretWordToTest);
 
-        //Assert
+        
         Assert.Contains("Invalid Secret Word: Word should have only letters", result);
     }
 
     [Fact]
     public void Should_ReturnValid_SecretWord()
     {
-        //Arrange
+        
         var secretWordToTest = "plane";
         Wordle wordle = new();
 
-        //Act
-        var result = wordle.validateSecretWord(secretWordToTest);
+        
+        var result = wordle.ValidateSecretWord(secretWordToTest);
 
-        //Assert
+        
         Assert.Contains("Let's Play!", result);
     }
     
@@ -83,61 +83,61 @@ public class WordleProjectTest
      [Fact]
     public void Should_ReturnInvalid_NoGuess()
     {
-        //Arrange
+        
         var secretWordToTest = "plane";
         var guessedWord = "";
         Wordle wordle = new();
 
-        //Act
+        
         //refactored slightly to remove secretWord handling. I suspect in a reality situation I would have designed this to hold the secret word somewhere and it'd be handed in.
-        var result = wordle.validateGuess(guessedWord, secretWordToTest);
+        var result = wordle.ValidateGuess(guessedWord, secretWordToTest);
 
-        //Assert
+        
         Assert.Equal("Invalid Guess: No Word Supplied", result);
      }
 
     [Fact]
     public void Should_ReturnInvalid_ShortGuess()
     {
-        //Arrange
+        
         var secretWordToTest = "plane";
         var guessedWord = "plan";
         Wordle wordle = new();
 
-        //Act
-        var result = wordle.validateGuess(guessedWord, secretWordToTest);
+        
+        var result = wordle.ValidateGuess(guessedWord, secretWordToTest);
 
-        //Assert
+        
         Assert.Equal("Invalid Guess: guess too short", result);
      }
 
     [Fact]
     public void Should_ReturnInvalid_LongGuess()
     {
-        //Arrange
+        
         var secretWordToTest = "plane";
         var guessedWord = "planet";
         Wordle wordle = new();
 
-        //Act
-        var result = wordle.validateGuess(guessedWord, secretWordToTest);
+        
+        var result = wordle.ValidateGuess(guessedWord, secretWordToTest);
 
-        //Assert
+        
         Assert.Equal("Invalid Guess: guess too long" , result);
      }
 
     [Fact]
     public void Should_ReturnInvalid_DigitGuess()
     {
-        //Arrange
+        
         var secretWordToTest = "plane";
         var guessedWord = "p1ane";
         Wordle wordle = new();
 
-        //Act
-        var result = wordle.validateGuess(guessedWord, secretWordToTest);
+        
+        var result = wordle.ValidateGuess(guessedWord, secretWordToTest);
 
-        //Assert
+        
         Assert.Equal("Invalid Guess: Word should have only letters", result);
 
     }
@@ -145,75 +145,75 @@ public class WordleProjectTest
     [Fact]
     public void Should_ReturnGGGGG_CorrectGuess()
     {
-        //Arrange
+        
         var secretWordToTest = "plane";
         var guessedWord = "plane";
         Wordle wordle = new();
 
-        //Act
-        var result = wordle.validateGuess(guessedWord, secretWordToTest);
+        
+        var result = wordle.ValidateGuess(guessedWord, secretWordToTest);
 
-        //Assert  
+          
         Assert.Equal("GGGGG", result);
     }
 
     [Fact]
     public void Should_ReturnNNNNN_IncorrectGuess()
     {
-        //Arrange
+        
         var secretWordToTest = "plane";
         var guessedWord = "ditch";
         Wordle wordle = new();
         
-        //Act
-        var result = wordle.validateGuess(guessedWord,secretWordToTest);
+        
+        var result = wordle.ValidateGuess(guessedWord,secretWordToTest);
 
-        //Assert        
+                
         Assert.Equal("-----", result);
     }
 
     [Fact]
     public void Should_ReturnNNNYN_PartialCorrectLetterPoorlyPlacedGuess()
     {
-        //Arrange
+        
         var secretWordToTest = "plane";
         var guessedWord = "world";
         Wordle wordle = new();
 
-        //Act
-        var result = wordle.validateGuess(guessedWord, secretWordToTest);
+        
+        var result = wordle.ValidateGuess(guessedWord, secretWordToTest);
 
-        //Assert        
+                
         Assert.Equal("---Y-", result);
     }
 
     [Fact]
     public void Should_ReturnYYNNN_PartialCorrectMultiLetterGuess()
     {
-        //Arrange
+        
         var secretWordToTest = "plane";
         var guessedWord = "lever";
         Wordle wordle = new();
 
-        //Act
-        var result = wordle.validateGuess(guessedWord, secretWordToTest);
+        
+        var result = wordle.ValidateGuess(guessedWord, secretWordToTest);
 
-        //Assert
+        
         Assert.Equal("YY---", result);
     }
 
     [Fact]
     public void Should_ReturnValid_PartialCorrectGuess()
     {
-        //Arrange
+        
         var secretWordToTest = "plane";
         var guessedWord = "elate";
         Wordle wordle = new();
 
-        //Act
-        var result = wordle.validateGuess(guessedWord, secretWordToTest);
+        
+        var result = wordle.ValidateGuess(guessedWord, secretWordToTest);
 
-        //Assert
+        
         Assert.Equal("-GG-G", result);
     }
 
